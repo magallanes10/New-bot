43c5c9dce8850756a0abd7252d535aa9c8536c7a
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-01-22 18:32:40 +0800

    Update ai.js

--- script/commands/ai.js
+++ script/commands/ai.js
@@ -1,33 +1,32 @@
 const axios = require('axios');
 
 module.exports.config = {
-	name: "ai",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "Jonell Magallanes",
-	description: "EDUCATIONAL",
-	usePrefix: false,
-	commandCategory: "other",
-	usages: "[question]",
-	cooldowns: 10
+    name: "ai",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes",
+    description: "EDUCATIONAL",
+    usePrefix: false,
+    commandCategory: "other",
+    usages: "[question]",
+    cooldowns: 10
 };
 
-module.exports.run = async function({ api, event, args }) {
-	const query = encodeURIComponent(args.join(" "));
-	const apiUrl = `https://cyni-api-collection.onrender.com/api/gpt?question=${query}`;
-if (!query) return api.sendMessage("Please Provide Your Question\n\nExample: ai what is solar system?", event.threadID, event.messageID);
-	
-	
-	try {
-		api.sendMessage("🔍 | AI is searching for your answer. Please wait...", event.threadID, event.messageID);
+module.exports.run = async function ({ api, event, args }) {
+    const content = encodeURIComponent(args.join(" "));
+    const apiUrl = `https://ai-chat-gpt-4-lite.onrender.com/api/hercai?question=${content}`;
 
-		const response = await axios.get(apiUrl);
-		const { ai } = response.data;
+    if (!content) return api.sendMessage("Please provide your question.\n\nExample: ai what is the solar system?", event.threadID, event.messageID);
 
-		api.sendMessage(ai, event.threadID, event.messageID);
-	} catch (error) {
-		console.error(error);
-		api.sendMessage("An error occurred while processing your request.", event.threadID);
-	}
+    try {
+        api.sendMessage("🔍 | AI is searching for your answer. Please wait...", event.threadID, event.messageID);
+
+        const response = await axios.get(apiUrl);
+        const { reply, requestNumber } = response.data;
+
+        api.sendMessage(`${reply}\n\n📝 Request count: ${requestNumber}`, event.threadID, event.messageID);
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
 };
-    
